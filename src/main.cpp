81962593e02de48654bd489a0ea8772749882580
Mihhai <micmihai97@protonmail.com>
2026-03-16 21:22:09 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ int main() {
   while (true) {
 	  std::cout << "$ ";
 	  std::getline(std::cin, command);
+	  auto first_word_end_index = command.find(" ");
+
+	  auto first_word = command.substr(0, first_word_end_index);
+	  if (first_word == "echo")
+	  {
+		  std::cout << command.substr(first_word_end_index + 1 , command.size()) << "\n";
+		  continue;
+	  }
 
 	  if (command == "exit")
 		  break;
