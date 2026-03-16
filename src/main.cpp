6fe6ef0c28ddfd4defc9fb19b5007ff65c3513e5
Mihhai <micmihai97@protonmail.com>
2026-03-16 21:43:17 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,29 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::string command;
+  std::string input;
+
   while (true) {
 	  std::cout << "$ ";
-	  std::getline(std::cin, command);
-	  auto first_word_end_index = command.find(" ");
+	  std::getline(std::cin, input);
+	  std::string command;
 
-	  auto first_word = command.substr(0, first_word_end_index);
-	  if (first_word == "echo")
-	  {
-		  std::cout << command.substr(first_word_end_index + 1 , command.size()) << "\n";
-		  continue;
+	  auto space_index = input.find(" ");
+
+	  if (space_index == std::string::npos) {
+		  command = input;
+	  }
+	  else {
+		  command = input.substr(0, space_index);
 	  }
 
 	  if (command == "exit")
 		  break;
+	  else if (command == "echo")
+	  {
+		  std::cout << input.substr(space_index + 1) << "\n";
+		  continue;
+	  }
 
 	  std::cout << command << ": " << "not found\n";
   }
