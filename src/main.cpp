ae5971d5c24f2e1e9c1c01da1394dc8755fb4639
Mihhai <micmihai97@protonmail.com>
2026-03-16 21:11:43 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
   std::string command;
-  std::getline(std::cin, command);
-
-  std::cout << command << ": " << "not found\n";
+  while (true) {
+	  std::cout << "$ ";
+	  std::getline(std::cin, command);
+	  std::cout << command << ": " << "not found\n";
+  }
 }
